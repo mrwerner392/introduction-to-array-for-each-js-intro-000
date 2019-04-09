ac1ea7a5d68f7a356a22a6eceb89b897ec028220
Matt Werner <mrwerner392@gmail.com>
2019-04-09 16:37:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-// Add your doToElementsInArray() function here:
+// calls .forEach on the given array using the given callback function
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 
-
-// Add your changeCompletely() function here:
+// turns every element in an array into a new string
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
